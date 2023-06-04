7c3c05002e9d1b6ce912eb193ac5abb4a9e08712
Jerry Pao <kyjerry0424@gmail.com>
2023-06-03 22:26:12 -0400

    define create collection route and controller function

--- controllers/collections.js
+++ controllers/collections.js
@@ -1,6 +1,17 @@
 const { Collection } = require('../models')
 
+const create = async (req,res) => {
+    try {
+        req.body.profileId = req.user.profile.id
+        console.log(req.body)
+        const collection = await Collection.create(req.body)
+        console.log(collection)
+        res.status(200).json(collection)
+    } catch (error) {
+        res.status(500).json({ err: error })
+    }
+}
 
 module.exports ={
-    
+    create
 }
--- routes/collections.js
+++ routes/collections.js
@@ -9,6 +9,7 @@ const { decodeUserFromToken, checkAuth } = middleware
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
+router.post('/', checkAuth, collectionsCtrl.create)
 
 
 module.exports = router
