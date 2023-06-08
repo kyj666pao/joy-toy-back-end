bbdca41b380edbcd8ae49f7970cbf526736ce085
Jerry Pao <kyjerry0424@gmail.com>
2023-06-08 19:24:53 -0400

    delete console log

--- controllers/collections.js
+++ controllers/collections.js
@@ -5,19 +5,13 @@ const cloudinary = require('cloudinary').v2
 const addPhoto = async (req, res) => {
     try {
         const { collectionId } = req.params
-        console.log("1---collectionId", collectionId)
-        console.log("2", req.files.img.path)
         const imageFile = req.files.img.path
-        console.log("3", imageFile)
         const collection = await Collection.findByPk(collectionId)
-        console.log("4", collection)
         const image = await cloudinary.uploader.upload(
             imageFile, 
             { tags: `${req.body.img}` }
         )
-        console.log("5---Here---", image)
         collection.img = image.url
-        console.log("6---",collection.img)
         await collection.save()
         res.status(201).json(collection.img)
     } catch (error) {
@@ -39,6 +33,19 @@ const create = async (req,res) => {
 const index = async (req, res) => {
     try {
         const collections = await Collection.findAll({
+            order: [ [ 'createdAt', 'DESC' ]]
+        })
+        res.status(200).json(collections)
+    } catch (error) {
+        res.status(500).json({ err: error })
+    }
+}
+
+const indexOnesCollections = async (req, res) => {
+    try {
+        const { id } = req.params
+        const collections = await Collection.findAll({
+            where: { profileId: id},
             order: [ [ 'createdAt', 'DESC' ]]
         })
         res.status(200).json(collections)
@@ -91,6 +98,7 @@ const deleteCollection = async (req, res) => {
 module.exports ={
     create,
     index,
+    indexOnesCollections,
     show,
     update,
     addPhoto,
--- routes/profiles.js
+++ routes/profiles.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const profilesCtrl = require('../controllers/profiles.js')
+const collectionsCtrl = require('../controllers/collections.js')
 const middleware = require('../middleware/auth.js')
 
 const { decodeUserFromToken, checkAuth } = middleware
@@ -11,5 +12,6 @@ const { decodeUserFromToken, checkAuth } = middleware
 router.use(decodeUserFromToken)
 router.get('/', checkAuth, profilesCtrl.index)
 router.put('/:id/add-photo', checkAuth, profilesCtrl.addPhoto)
+router.get('/:id', checkAuth, collectionsCtrl.indexOnesCollections)
 
 module.exports = router
